43897e24564c46b88e230b98464e27760f0fb764
LPerryman <45283878+LPerryman@users.noreply.github.com>
2020-08-17 13:45:12 +0100

    Delete main.py

--- datasets/HM-Land-Registry-Price-Paid-Data/main.py
+++
@@ -1,9 +0,0 @@
-etl_title = 'HM Land Registry Price Paid Data' 
-
-print(etl_title) 
-from gssutils import * 
-import json 
-
-info = json.load(open('info.json')) 
-scraper = Scraper(seed="info.json")   
-scraper 
