51d5cf4792e67f963e13ddd9895d3084a36e3851
LPerryman <leigh.perryman@gsscogs.uk>
2020-09-15 15:07:19 +0100

    CHanging the title slightly

--- datasets/ONS-Population-estimates-by-output-areas-electoral-health-and-other-geographies-England-and-Wales/main.py
+++ datasets/ONS-Population-estimates-by-output-areas-electoral-health-and-other-geographies-England-and-Wales/main.py
@@ -39,6 +39,7 @@ from urllib.parse import urljoin
 
 scraper.dataset.family = 'towns-high-streets'
 scraper.dataset.comment = 'Estimates of the population for England and Wales by Geographical Output areas, Gender and Age'
+scraper.dataset.title = 'Population estimates by output areas, England and Wales'
 
 dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}'))
 scraper.set_base_uri('http://gss-data.org.uk')
