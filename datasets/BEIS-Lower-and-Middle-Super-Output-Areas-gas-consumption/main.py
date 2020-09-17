7dfc1027d910310d9c37ce1033e0fdeb9afa7e7a
LPerryman <leigh.perryman@gsscogs.uk>
2020-09-17 15:13:37 +0100

    STage 2 done without a spec as quite straightforward

--- datasets/BEIS-Lower-and-Middle-Super-Output-Areas-gas-consumption/main.py
+++ datasets/BEIS-Lower-and-Middle-Super-Output-Areas-gas-consumption/main.py
@@ -138,13 +138,85 @@ for distribution in scraper.distributions:
 
                     trace.store(file_name, table)
                     tidied_sheets[file_name] = table
-                    
 
+
+# +
+#for key in tidied_sheets:
+#    print(key)
+# -
+
+trace.render("spec_v1.html")
+
+# Only output LSOA data for now until PMD4 can handle multiple outputs
+lsoa_dat = pd.DataFrame(columns=tidied_sheets['LSOA_GAS_2018'].columns)
+for key in tidied_sheets:
+    if 'LSOA' in key:
+        print('joining: ' + key)
+        lsoa_dat = pd.concat([lsoa_dat,pd.DataFrame(tidied_sheets[key])], sort=False)
+
+# Remove attributes for now until we dicide whow we are handling them
+del lsoa_dat['LAName']
+del lsoa_dat['MSOAName']
+del lsoa_dat['LSOAName']
+del lsoa_dat['METERS']
+del lsoa_dat['MEAN']
+del lsoa_dat['MEDIAN']
+
+# +
+#Rename the columns to match the Electricity pipeline
+'''
+lsoa_dat = lsoa_dat.rename(columns=
+                           {
+                               'METERS': 'Total number of domestic electricity meters', 
+                               'MEAN': 'Mean domestic electricity consumption kWh per meter',
+                               'MEDIAN': 'Median domestic electricity consumption kWh per meter'
+                           })
+'''
+
+
+lsoa_dat = lsoa_dat.rename(columns=
+                           {
+                               'Period': 'Year',
+                               'LACode': 'Local Authority', 
+                               'MSOACode': 'Middle Layer Super Output Area',
+                               'LSOACode': 'Lower Layer Super Output Area'
+                           })
+lsoa_dat['Year'] = 'year/' + lsoa_dat['Year'].astype(str)
+# -
+
+lsoa_dat.head(10)
+
+# +
+import os
+from urllib.parse import urljoin
+
+notes = 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/853104/sub-national-methodology-guidance.pdf'
+
+csvName = 'lsoa_observations.csv'
 out = Path('out')
 out.mkdir(exist_ok=True)
+df.drop_duplicates().to_csv(out / csvName, index = False)
 
-for key in tidied_sheets:
-    df = tidied_sheets[key]
-    df.drop_duplicates().to_csv(out / f'{key}.csv', index=False)
-    
-trace.render("spec_v1.html")
+scraper.dataset.family = 'towns-high-streets'
+scraper.dataset.description = scraper.dataset.description + '\nGuidance documentation can be found here:\n' + notes
+#scraper.dataset.comment = 'Total domestic gas consumption, number of meters, mean and median consumption for LSOA regions across England, Wales & Scotland'
+scraper.dataset.comment = 'Total domestic gas consumption for LSOA regions across England, Wales & Scotland'
+scraper.dataset.title = 'Lower Super Output Areas (LSOA) gas consumption'
+
+dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower()
+scraper.set_base_uri('http://gss-data.org.uk')
+scraper.set_dataset_id(dataset_path)
+
+
+csvw_transform = CSVWMapping()
+csvw_transform.set_csv(out / csvName)
+csvw_transform.set_mapping(json.load(open('info.json')))
+csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
+csvw_transform.write(out / f'{csvName}-metadata.json')
+# Remove subset of data
+#out / csvName).unlink()
+with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
+    metadata.write(scraper.generate_trig())
+# -
+
+
