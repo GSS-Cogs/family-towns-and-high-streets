fa8eb9894959a8a5ccef39a8d0cd55d4f417ebe4
LPerryman <leigh.perryman@gsscogs.uk>
2020-10-16 12:36:35 +0100

    Now outputting all 3 datasets ready for testing

--- datasets/NHS-D-Patients-Registered-at-a-GP-Practice/main.py
+++ datasets/NHS-D-Patients-Registered-at-a-GP-Practice/main.py
@@ -304,7 +304,7 @@ gp_practice.drop_duplicates().to_csv(out / csvName, index = False)
 scraper.dataset.family = 'towns-high-streets'
 scraper.dataset.description = notes
 scraper.dataset.comment = 'Data for this publication are extracted each month as a snapshot in time from the Primary Care Registration database within the NHAIS (National Health Application and Infrastructure Services) system.'
-scraper.dataset.title = 'Patients Registered at a GP Practice - '
+scraper.dataset.title = 'Patients Registered at a GP Practice - GP'
 
 dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name) + '/gp').lower()
 scraper.set_base_uri('http://gss-data.org.uk')
@@ -318,45 +318,188 @@ csvw_transform.write(out / f'{csvName}-metadata.json')
 
 with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
     metadata.write(scraper.generate_trig())
+# -
 
-# +
-#del tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)']['PUBLICATION']
-#del tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']['PUBLICATION']
+del tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)']['PUBLICATION']
+del tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']['PUBLICATION']
+del tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']['Post Code']
 
-# +
-#tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'] = tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'].rename(columns={
-#    'ORG_TYPE': 'ORG Type',
-#    'ORG_CODE': 'ORG Code',
-#    'ONS_CODE': 'ONS Code',
-#    'SEX': 'Sex',
-#    'AGE': 'Age'
-#})
-
-
-
-# +
-#tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)'].head(10)
-#tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']['ORG Type'].unique()
-
-# +
-#d = tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']
-#d = d[d['Post Code'].notnull()]
+# Get rid of the GP data because we have added to to the first dataset
+other5yr = tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']
+other5yr = other5yr[other5yr['ORG Type'] != 'GP']
+tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)'] = other5yr
 #tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)'].head(10)
 
-# +
-#d['ORG Type'].unique()
+tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'] = tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'].rename(columns={
+    'ORG_TYPE': 'ORG Type',
+    'ONS_CODE': 'ONS Code',
+    'ORG_CODE': 'ORG Code',
+    'SEX': 'Sex',
+    'AGE': 'Age'
+})
+#tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'].head(10)
 
 # +
-#list(tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)']['ORG_TYPE'].unique())
+org_practice = pd.concat([tidied_data['Single year of age (Commissioning Regions-STPs-CCGs-PCNs)'],
+                         tidied_data['5-year age groups (Commissioning Regions-STPs-CCGs-PCNs-GP practice)']
+                         ], sort=True)
+
+org_practice['Sex'] = org_practice['Sex'].replace({
+    'ALL':'T',
+    'FEMALE':'F',
+    'FEMALES':'F',
+    'MALE':'M',
+    'MALES':'M',
+    'UNKNOWN':'U'
+})
+
+# +
+#print('All: ' + str(org_practice['Period'].count()))
+
+#for c in org_practice:
+#    if c != 'Value':
+#        print('Column: ' + c)
+#        print('Row count: ' + str(org_practice[c].count()))
+#        print(list(org_practice[c].unique()))
+#        print("#######################################")
+
+# -
+
+# We can map CCG, STP and COMM regions to Geograpgy codes but not PCN.
+# So publish PCN on its own and the rest together
+print('Org data count before removing PCN data: ' + str(org_practice['Age'].count()))
+pcn_practice = org_practice[org_practice['ORG Type'] == 'PCN']
+print('PCN data count: ' + str(pcn_practice['Age'].count()))
+org_practice = org_practice[org_practice['ORG Type'] != 'PCN']
+print('Org data count after before removing PCN data: ' + str(org_practice['Age'].count()))
+
+# +
+# Pull in the mapping files and concat
+#ccgMap = pd.read_csv('../../Reference/nhs-ccg-map-to-geography.csv') 
+#stpMap = pd.read_csv('../../Reference/nhs-stp-map-to-geography.csv') 
+#comMap = pd.read_csv('../../Reference/nhs-commissioning-region-map-to-geography.csv') 
+#allMap = pd.concat([ccgMap, stpMap, comMap])
+# Map the 3 ORG codes (CCG, STP and COMM Region)
+#org_practice['ORG Code'] = org_practice['ORG Code'].map(allMap.set_index('NHS Code')['Geog Code'])
+# -
+
+del org_practice['ORG Code']
+org_practice['ORG Type'] = org_practice['ORG Type'].apply(pathify)
+org_practice = org_practice.rename(columns={'ONS Code': 'ONS ORG Code'})
+org_practice['Age'] = org_practice['Age'].str.replace('_','-') 
+org_practice['Age'] = org_practice['Age'].apply(pathify)
+org_practice['Period'] = 'day/' + org_practice['Period'].astype(str)
+org_practice.head(10)
+
+# +
+notes = f"""
+Data for this publication are extracted each month as a snapshot in time from the Primary Care Registration database within the NHAIS (National Health Application and Infrastructure Services) system.
+GP Practice; Primary Care Network (PCN); Sustainability and transformation partnership (STP); Clinical Commissioning Group (CCG) and NHS England Commissioning Region level data are released in single year of age (SYOA) and 5-year age bands, both of which finish at 95+, split by gender. In addition, organisational mapping data is available to derive STP; PCN; CCG and Commissioning Region associated with a GP practice and is updated each month to give relevant organisational mapping.
+Quarterly publications in January, April, July and October will include Lower Layer Super Output Area (LSOA) populations and a spotlight report.
+The outbreak of Coronavirus (COVID-19) has led to changes in the work of General Practices and subsequently the data within this publication. Until activity in this healthcare setting stabilises, we urge caution in drawing any conclusions from these data without consideration of the country
+"""
+
+csvName = 'org_observations.csv'
+out = Path('out')
+out.mkdir(exist_ok=True)
+org_practice.drop_duplicates().to_csv(out / csvName, index = False)
+#org_practice.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+
+scraper.dataset.family = 'towns-high-streets'
+scraper.dataset.description = notes
+scraper.dataset.comment = 'Data for this publication are extracted each month as a snapshot in time from the Primary Care Registration database within the NHAIS (National Health Application and Infrastructure Services) system.'
+scraper.dataset.title = 'Patients Registered at a GP Practice - CCG, STP, Comm Region'
+
+dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name) + '/org').lower()
+scraper.set_base_uri('http://gss-data.org.uk')
+scraper.set_dataset_id(dataset_path)
+
+csvw_transform = CSVWMapping()
+csvw_transform.set_csv(out / csvName)
+csvw_transform.set_mapping(json.load(open('info.json')))
+csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
+csvw_transform.write(out / f'{csvName}-metadata.json')
+
+with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
+    metadata.write(scraper.generate_trig())
+# -
+
+del pcn_practice['ONS Code']
+pcn_practice = pcn_practice.rename(columns={'ORG Code': 'PCN Code'})
+pcn_practice['Age'] = pcn_practice['Age'].str.replace('_','-') 
+pcn_practice['Age'] = pcn_practice['Age'].apply(pathify)
+pcn_practice['ORG Type'] = pcn_practice['ORG Type'].apply(pathify)
+pcn_practice['Period'] = 'day/' + pcn_practice['Period'].astype(str)
+pcn_practice.head()
+
+# +
+notes = f"""
+Data for this publication are extracted each month as a snapshot in time from the Primary Care Registration database within the NHAIS (National Health Application and Infrastructure Services) system.
+GP Practice; Primary Care Network (PCN); Sustainability and transformation partnership (STP); Clinical Commissioning Group (CCG) and NHS England Commissioning Region level data are released in single year of age (SYOA) and 5-year age bands, both of which finish at 95+, split by gender. In addition, organisational mapping data is available to derive STP; PCN; CCG and Commissioning Region associated with a GP practice and is updated each month to give relevant organisational mapping.
+Quarterly publications in January, April, July and October will include Lower Layer Super Output Area (LSOA) populations and a spotlight report.
+The outbreak of Coronavirus (COVID-19) has led to changes in the work of General Practices and subsequently the data within this publication. Until activity in this healthcare setting stabilises, we urge caution in drawing any conclusions from these data without consideration of the country
+"""
+
+csvName = 'pcn_observations.csv'
+out = Path('out')
+out.mkdir(exist_ok=True)
+pcn_practice.drop_duplicates().to_csv(out / csvName, index = False)
+#pcn_practice.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+
+scraper.dataset.family = 'towns-high-streets'
+scraper.dataset.description = notes
+scraper.dataset.comment = 'Data for this publication are extracted each month as a snapshot in time from the Primary Care Registration database within the NHAIS (National Health Application and Infrastructure Services) system.'
+scraper.dataset.title = 'Patients Registered at a GP Practice - PCN'
+
+dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name) + '/pcn').lower()
+scraper.set_base_uri('http://gss-data.org.uk')
+scraper.set_dataset_id(dataset_path)
+
+csvw_transform = CSVWMapping()
+csvw_transform.set_csv(out / csvName)
+csvw_transform.set_mapping(json.load(open('info.json')))
+csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
+csvw_transform.write(out / f'{csvName}-metadata.json')
+
+with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
+    metadata.write(scraper.generate_trig())
 
 # +
 #scraper.dataset.family = 'towns-high-streets'
-#codelistcreation = ['Age'] 
-#df = gp_practice
+#codelistcreation = ['ORG Code'] 
+#df = pcn_practice
 #codeclass = CSVCodelists()
 #for cl in codelistcreation:
 #    if cl in df.columns:
 #        codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists', scraper.dataset.family, Path(os.getcwd()).name.lower())
 # -
+"""
+import numpy as np
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+
+var_url = urlopen('https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?PrimaryRoleId=RO272&Limit=1000&_format=xml')
+xmldoc = parse(var_url)
+
+org_dat = pd.DataFrame(columns=['Label','Notation'])
+df2 = pd.DataFrame([[2,3,4]], columns=['A','B','C'])
+for item in xmldoc.iterfind('Organisation'):
+    orgID = item.findtext('OrgId')
+    orgName = item.findtext('Name')
+
+    df = pd.DataFrame([[orgName, orgID]], columns=['Label','Notation'])
+    org_dat = pd.concat([org_dat, df])
+    del df
+    #print(orgID + ' : ' + orgName)
+org_dat = pd.DataFrame(org_dat) 
+
+org_dat['Parent Notation'] = ''
+org_dat['Sort Priority'] = np.arange(len(org_dat)) + 1
+org_dat['Notation'] = org_dat['Notation'].apply(pathify)
+org_dat
+"""
+
+
+
 
 
