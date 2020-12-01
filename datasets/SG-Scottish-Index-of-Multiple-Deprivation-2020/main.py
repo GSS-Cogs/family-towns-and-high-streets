2569d008fc578ed6883dba4780883c6b430fd80d
RedWalters <jonathan.j.walters@gmail.com>
2020-12-01 11:26:51 +0000

    replacing seed with scraper

--- datasets/SG-Scottish-Index-of-Multiple-Deprivation-2020/main.py
+++ datasets/SG-Scottish-Index-of-Multiple-Deprivation-2020/main.py
@@ -1,63 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[64]:
+
+
 # -*- coding: utf-8 -*-
-from gssutils import * 
-import json 
+from gssutils import *
+import json
 import requests
 import os
-from urllib.parse import urljoin
-
-# +
-# Make sure the dataURL and Unit values are correctb to begin with
-with open("info.json", "r") as jsonFile:
-    data = json.load(jsonFile)
-
-data["dataURL"] = "https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/01/scottish-index-of-multiple-deprivation-2020-ranks-and-domain-ranks/documents/scottish-index-of-multiple-deprivation-2020-ranks-and-domain-ranks/scottish-index-of-multiple-deprivation-2020-ranks-and-domain-ranks/govscot%3Adocument/SIMD%2B2020v2%2B-%2Branks.xlsx"
-data["transform"]["columns"]["Value"]["unit"] = "http://gss-data.org.uk/def/concept/measurement-units/ranks"
-
-with open("info.json", "w") as jsonFile:
-    json.dump(data, jsonFile)
-# -
+from urllib.parse import urljoin, urlparse
 
 trace = TransformTrace()
 tidied_sheets = {}
-scraper = Scraper(seed="info.json")   
-scraper.distributions[0].title = "Scottish Index of Multiple Deprivation 2020"
-scraper 
+#scraper = Scraper(seed="info.json")
+info = json.load(open('info.json'))
+scraper = Scraper(info['landingPage'])
 
-# +
+for i in scraper.distributions:
+    print(i.title)
+    print(i.downloadURL)
+    print(i.mediaType)
+
+
+# In[65]:
+
+
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+"""info = json.load(open('info.json'))
+
+req = Request(info["landingPage"], headers={'User-Agent': 'Mozilla/5.0'})
+html = urlopen(req).read()
+plaintext = html.decode('utf8')
+soup = BeautifulSoup(plaintext, 'html.parser')
+
+
+pubPages = soup.findAll('ul', class_= 'collections-list')
+for page in pubPages:
+    pubs = page.findAll('a')
+    for i in pubs:
+        print(urljoin("https://www.gov.scot", i['href']))"""
+
+
+# In[66]:
+
+
 #tabs = { tab for tab in scraper.distributions[0].as_databaker() }
 #len(tabs)
-# -
+
+
+# In[67]:
+
 
 
 """
 for tab in tabs:
     if 'SIMD 2020v2 ranks' == tab.name:
         table = tab
-        
+
 columns=["Data Zone", "Intermediate Zone", "Council Area", "Total Population", "Working Age Population", "Rank Type"]
 trace.start(scraper.distributions[0].title, table, columns, scraper.distributions[0].downloadURL)
-
 data_zone = table.filter(contains_string('Data_Zone')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Data_Zone("Selected as all non blank values from cell ref A2 down")
-
 intermediate_zone = table.filter(contains_string('Intermediate_Zone')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Intermediate_Zone("Selected as all non blank values from cell ref B2 down")
-
 council_area = table.filter(contains_string('Council_area')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Council_Area("Selected as all non blank values from cell ref C2 down")
-
 total_population = table.filter(contains_string('Total_population')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Total_Population("Selected as all non blank values from cell ref D2 down")
-
 working_age_population = table.filter(contains_string('Working_age_population')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Working_Age_Population("Selected as all non blank values from cell ref E2 down")
-
 rank_type = table.filter(contains_string('SIMD2020v2_Rank')).expand(RIGHT).is_not_blank()
 trace.Rank_Type("Selected as all non blank values from cell ref F3 right")
 observation = table.filter(contains_string('SIMD2020v2_Rank')).shift(0,1).expand(DOWN).expand(RIGHT).is_not_blank()
-
-
-
 dimensions = [
     HDim(data_zone, 'Data Zone', DIRECTLY, LEFT),
     HDim(intermediate_zone, 'Intermediate Zone', DIRECTLY, LEFT),
@@ -66,63 +85,50 @@ dimensions = [
     HDim(working_age_population, 'Working Age Population', DIRECTLY, LEFT),
     HDim(rank_type, 'Rank Type', DIRECTLY, ABOVE)
 ]
-
-
 tidy_sheet = ConversionSegment(table, dimensions, observation)
 # savepreviewhtml(tidy_sheet)
 trace.with_preview(tidy_sheet)
-
 trace.store("ranks_dataframe", tidy_sheet.topandas())
 tidied_sheets["SIMD 2020v2 ranks"] = tidy_sheet.topandas()
 """
 """
 url = 'https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/01/scottish-index-of-multiple-deprivation-2020-indicator-data/documents/simd_2020_indicators/simd_2020_indicators/govscot%3Adocument/SIMD%2B2020v2%2B-%2Bindicators.xlsx'
 r = requests.get(url, allow_redirects=True)
-
 open('indicators.xls', 'wb').write(r.content)
 tabs = loadxlstabs('indicators.xls')
-
 trace = TransformTrace()
 """
 """
 for tab in tabs:
     if tab.name == 'Data':
         table = tab
-
 title = scraper.distributions[0].title + "Indicators"
 columns=["Data Zone", "Intermediate Zone", "Council Area", "Indicator Type"]
 trace.start(title, table, columns, url)
-
 data_zone = table.filter(contains_string('Data_Zone')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Data_Zone("Selected as all non blank values from cell ref A2 down")
-
 intermediate_zone = table.filter(contains_string('Intermediate_Zone')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Intermediate_Zone("Selected as all non blank values from cell ref B2 down")
-
 council_area = table.filter(contains_string('Council_area')).shift(0,1).expand(DOWN).is_not_blank()
 trace.Council_Area("Selected as all non blank values from cell ref C2 down")
-
 observation = table.filter(contains_string('Total_population')).shift(0,1).expand(DOWN).expand(RIGHT).is_not_blank()
-
 indicator = table.filter(contains_string('Total_population')).expand(RIGHT).is_not_blank()
 trace.Council_Area("Selected as all non blank values from cell ref D1 right")
-
-
 dimensions = [
     HDim(data_zone, 'Data Zone', DIRECTLY, LEFT),
     HDim(intermediate_zone, 'Intermediate Zone', DIRECTLY, LEFT),
     HDim(council_area, 'Council Area', DIRECTLY, LEFT),
     HDim(indicator, 'Indicator Type', DIRECTLY, ABOVE)
 ]
-
 tidy_sheet = ConversionSegment(table, dimensions, observation)
 # savepreviewhtml(tidy_sheet)
 trace.with_preview(tidy_sheet)
-
 trace.store("indicators_dataframe", tidy_sheet.topandas())
 tidied_sheets["Data"] = tidy_sheet.topandas()
 """
 
+
+# In[68]:
 
 
 # Sheet names
@@ -141,8 +147,8 @@ de = [
 ]
 # Title
 ti = [
-    'Scottish Index of Multiple Deprivation - Ranks',
-    'Scottish Index of Multiple Deprivation - Indicators'
+    'Scottish Index of Multiple Deprivation - Ranks', 'Scottish Index of Multiple Deprivation - Ranks',
+    'Scottish Index of Multiple Deprivation - Ranks', 'Scottish Index of Multiple Deprivation - Indicators'
 ]
 # Paths
 pa = ['/ranks', '/indicators']
@@ -166,7 +172,7 @@ try:
         if k == 0:
             joined_dat = t
         else:
-            joined_dat = pd.concat([joined_dat,t])   
+            joined_dat = pd.concat([joined_dat,t])
         k = k + 1
 
     joined_dat['Deprivation Rank'] = joined_dat['Deprivation Rank'].apply(pathify)
@@ -174,9 +180,8 @@ try:
     csvName = fn[i]
     out = Path('out')
     out.mkdir(exist_ok=True)
-    joined_dat[:5].drop_duplicates().to_csv(out / csvName, index = False)
-    joined_dat.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
-    
+    joined_dat.drop_duplicates().to_csv(out / csvName, index = False)
+
     scraper.dataset.family = 'towns-high-streets'
     scraper.dataset.description = scraper.dataset.description + '\n' + de[i]
     scraper.dataset.comment = co[i]
@@ -186,14 +191,12 @@ try:
     scraper.set_base_uri('http://gss-data.org.uk')
     scraper.set_dataset_id(dataset_path)
 
+
     csvw_transform = CSVWMapping()
     csvw_transform.set_csv(out / csvName)
     csvw_transform.set_mapping(json.load(open('info.json')))
     csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
     csvw_transform.write(out / f'{csvName}-metadata.json')
-
-    # Remove subset of data
-    (out / csvName).unlink()
 
     with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
         metadata.write(scraper.generate_trig())
@@ -202,7 +205,10 @@ except Exception as s:
 
 del joined_dat
 
-# +
+
+# In[69]:
+
+
 # need to change the dataURLa to the indicators one
 with open("info.json", "r") as jsonFile:
     data = json.load(jsonFile)
@@ -211,15 +217,18 @@ data["dataURL"] = "https://www.gov.scot/binaries/content/documents/govscot/publi
 data["transform"]["columns"]["Value"]["unit"] = "http://gss-data.org.uk/def/concept/measurement-units/indicators"
 
 with open("info.json", "w") as jsonFile:
-    json.dump(data, jsonFile)
-    
+    json.dump(data, jsonFile, indent = 2)
+
 trace = TransformTrace()
 tidied_sheets = {}
-scraper = Scraper(seed="info.json")   
+scraper = Scraper(seed="info.json")
 scraper.distributions[0].title = "Scottish Index of Multiple Deprivation 2020"
-scraper 
+scraper
 
-# +
+
+# In[70]:
+
+
 try:
     i = 1
     tab = scraper.distributions[0].as_pandas(sheet_name=sn[i])
@@ -239,11 +248,11 @@ try:
         if k == 0:
             joined_dat = t
         else:
-            joined_dat = pd.concat([joined_dat,t])   
+            joined_dat = pd.concat([joined_dat,t])
         k = k + 1
 
     joined_dat["Indicator Type"] = joined_dat["Deprivation Indicator"]
-    joined_dat["High Level Domain"] = joined_dat["Deprivation Indicator"]
+    joined_dat["High Level Indicator"] = joined_dat["Deprivation Indicator"]
 
     joined_dat['Indicator Type'] = joined_dat['Indicator Type'].replace({
         'Income rate':'Percentage',
@@ -280,7 +289,7 @@ try:
         'nocentralheat rate':'Percentage'
      })
 
-    joined_dat['High Level Domain'] = joined_dat['High Level Domain'].replace({
+    joined_dat['High Level Indicator'] = joined_dat['High Level Indicator'].replace({
         'Income rate':'Income',
         'Income count':'Income',
         'Employment rate':'Employment',
@@ -316,20 +325,19 @@ try:
      })
     joined_dat['Deprivation Indicator'] = joined_dat['Deprivation Indicator'].apply(pathify)
     joined_dat['Indicator Type'] = joined_dat['Indicator Type'].apply(pathify)
-    joined_dat['High Level Domain'] = joined_dat['High Level Domain'].apply(pathify)
+    joined_dat['High Level Indicator'] = joined_dat['High Level Indicator'].apply(pathify)
 
     joined_dat['Marker'] = ''
     joined_dat['Marker'][joined_dat['Value'] == '*'] = 'suppressed-or-population-zero'
     joined_dat['Value'][joined_dat['Value'] == '*'] = 0
-    
-    joined_dat = joined_dat[['Data Zone','Deprivation Indicator','Indicator Type','High Level Domain','Total population','Working age population','Marker','Value']]
-    
+
+    jouned_dat = joined_dat[['Data Zone','Deprivation Indicator','Indicator Type','High Level Indicator','Total population','Working age population','Marker','Value']]
+
     csvName = fn[i]
     out = Path('out')
     out.mkdir(exist_ok=True)
-    joined_dat[:5].drop_duplicates().to_csv(out / csvName, index = False)
-    joined_dat.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
-    
+    joined_dat.drop_duplicates().to_csv(out / csvName, index = False)
+
     scraper.dataset.family = 'towns-high-streets'
     scraper.dataset.description = scraper.dataset.description + '\n' + de[i]
     scraper.dataset.comment = co[i]
@@ -346,19 +354,22 @@ try:
     csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
     csvw_transform.write(out / f'{csvName}-metadata.json')
 
-    # Remove subset of data
-    (out / csvName).unlink()
-
     with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
         metadata.write(scraper.generate_trig())
 
 except Exception as s:
     print(str(s))
 
-# +
-#joined_dat.head(20)
 
-# +
+# In[71]:
+
+
+joined_dat.head(20)
+
+
+# In[72]:
+
+
 # Need to change the dataURL back to the RANK URL ready for the next run
 with open("info.json", "r") as jsonFile:
     data = json.load(jsonFile)
@@ -367,11 +378,14 @@ data["dataURL"] = "https://www.gov.scot/binaries/content/documents/govscot/publi
 data["transform"]["columns"]["Value"]["unit"] = "http://gss-data.org.uk/def/concept/measurement-units/ranks"
 
 with open("info.json", "w") as jsonFile:
-    json.dump(data, jsonFile)
+    json.dump(data, jsonFile, indent = 2)
 
-# +
+
+# In[73]:
+
+
 #scraper.dataset.family = 'towns-high-streets'
-#codelistcreation = ['Deprivation Indicator'] 
+#codelistcreation = ['Deprivation Indicator']
 #df = joined_dat
 #codeclass = CSVCodelists()
 #for cl in codelistcreation:
@@ -380,11 +394,4 @@ with open("info.json", "w") as jsonFile:
 #        df[cl] = df[cl].str.capitalize()
 #        codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists', scraper.dataset.family, Path(os.getcwd()).name.lower())
 # -
-
-
-
-
-
-
-
 
