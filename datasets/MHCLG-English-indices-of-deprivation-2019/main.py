567941d3fc869e67b642e5ac4d043b63e043eae9
RedWalters <jonathan.j.walters@gmail.com>
2021-01-25 11:52:19 +0000

    commenting out scrape for up to date data URL to see if its causing the hangup on jenkins

--- datasets/MHCLG-English-indices-of-deprivation-2019/main.py
+++ datasets/MHCLG-English-indices-of-deprivation-2019/main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[19]:
 
 
 #!/usr/bin/env python
 # coding: utf-8
 
 
-# In[10]:
-
-
-
-
-
-# In[11]:
+# In[20]:
 
 
 from gssutils import *
@@ -27,6 +21,8 @@ def all_same(items):
         return False
     return all(x == items[0] for x in items)
 
+
+cubes = Cubes("info.json")
 info = json.load(open('info.json'))
 etl_title = info["title"]
 etl_publisher = info["publisher"][0]
@@ -34,42 +30,43 @@ print("Publisher: " + etl_publisher)
 print("Title: " + etl_title)
 
 
-# In[12]:
+# In[21]:
 
 
+#Commenting out Data URL retrieve to see if its the issue on the pipeline
 
-req = Request("https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019", headers={'User-Agent': 'Mozilla/5.0'})
-html = urlopen(req).read()
-plaintext = html.decode('utf8')
-links = re.findall("href=[\"\'](.*?)[\"\']", plaintext)
-links = [x for x in links if x.endswith("csv")]
-if all_same(links) == True:
-    dataURL = links[0]
-    with open('info.json', 'r+') as info:
-        data = json.load(info)
-        data["dataURL"] = dataURL
-        info.seek(0)
-        json.dump(data, info, indent=4)
-        info.truncate()
-        print("Data URL found and added to info.json")
-else:
-    print("Multiple or No files found, investigate or retrieve manually")
-    for i in links:
-        print(i)
+#req = Request("https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019", headers={'User-Agent': 'Mozilla/5.0'})
+#html = urlopen(req).read()
+#plaintext = html.decode('utf8')
+#links = re.findall("href=[\"\'](.*?)[\"\']", plaintext)
+#links = [x for x in links if x.endswith("csv")]
+#if all_same(links) == True:
+#    dataURL = links[0]
+#    with open('info.json', 'r+') as info:
+#        data = json.load(info)
+#        data["dataURL"] = dataURL
+#        info.seek(0)
+#        json.dump(data, info, indent=4)
+#        info.truncate()
+#        print("Data URL found and added to info.json")
+#else:
+#    print("Multiple or No files found, investigate or retrieve manually")
+#    for i in links:
+#        print(i)
 
 scraper = Scraper(seed = "info.json")
 scraper.distributions[0].title = etl_title
 scraper
 
 
-# In[13]:
+# In[22]:
 
 
 tab = scraper.distributions[0].as_pandas()
 #tab
 
 
-# In[14]:
+# In[23]:
 
 
 tbls = []
@@ -83,7 +80,7 @@ for x in range(4, len(tab.columns)):
     tbls.append(dat)
 
 
-# In[15]:
+# In[24]:
 
 
 i = 0
@@ -95,14 +92,14 @@ for t in tbls:
     i = i + 1
 
 
-# In[16]:
+# In[25]:
 
 
 joined_dat['Index of Deprivation'] = joined_dat['Index of Deprivation'].apply(pathify)
 #joined_dat.head(20)
 
 
-# In[17]:
+# In[26]:
 
 
 import os
@@ -128,31 +125,43 @@ https://www.gov.uk/government/publications/english-indices-of-deprivation-2019-t
 """
 
 csvName = 'observations.csv'
-out = Path('out')
-out.mkdir(exist_ok=True)
-joined_dat.drop_duplicates().to_csv(out / csvName, index = False)
-joined_dat.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+#out = Path('out')
+#out.mkdir(exist_ok=True)
+#joined_dat.drop_duplicates().to_csv(out / csvName, index = False)
+#joined_dat.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
 
 scraper.dataset.family = 'towns-high-streets'
 scraper.dataset.description = notes
 scraper.dataset.comment = 'Statistics on relative deprivation in small areas in England, 2019.'
 scraper.dataset.title = 'English indices of deprivation'
 
-dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower()
-scraper.set_base_uri('http://gss-data.org.uk')
-scraper.set_dataset_id(dataset_path)
-
-csvw_transform = CSVWMapping()
-csvw_transform.set_csv(out / csvName)
-csvw_transform.set_mapping(json.load(open('info.json')))
-csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
-csvw_transform.write(out / f'{csvName}-metadata.json')
-
-with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
+cubes.add_cube(scraper, joined_dat.drop_duplicates(), csvName)
 
 
-# In[18]:
+# In[27]:
+
+
+cubes.output_all()
+
+
+# In[28]:
+
+
+#dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower()
+#scraper.set_base_uri('http://gss-data.org.uk')
+#scraper.set_dataset_id(dataset_path)
+
+#csvw_transform = CSVWMapping()
+#csvw_transform.set_csv(out / csvName)
+#csvw_transform.set_mapping(json.load(open('info.json')))
+#csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
+#csvw_transform.write(out / f'{csvName}-metadata.json')
+
+#with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
+#    metadata.write(scraper.generate_trig())
+
+
+# In[29]:
 
 
 """codelistcreation = ['Index of Deprivation']
@@ -165,13 +174,13 @@ for cl in codelistcreation:
         codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists', scraper.dataset.family, Path(os.getcwd()).name.lower())"""
 
 
-# In[18]:
+# In[29]:
 
 
 
 
 
-# In[18]:
+# In[29]:
 
 
 
