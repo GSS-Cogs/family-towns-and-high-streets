8fc60be08981de3666bc3f31271dc2b300f128a9
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-03-09 11:55:03 +0000

    1. Completed stage -1 2. Formatted Period column 3. Pathified Size Band column 5. Renamed OBS to Value 4. Added cubes class 5. Added TraceTransform

--- datasets/ONS-Towns-local-units-counts-by-size-bands-Inter-Departmental-Business-Register-IDBR-2015-to-2018/main.py
+++ datasets/ONS-Towns-local-units-counts-by-size-bands-Inter-Departmental-Business-Register-IDBR-2015-to-2018/main.py
@@ -8,6 +8,9 @@ import json
 scraper = Scraper(seed = 'info.json')
 scraper
 
+trace = TransformTrace()
+cubes = Cubes("info.json")
+
 # %%
 for i in scraper.distributions:
     display(i)
@@ -27,32 +30,69 @@ for tab in tabs:
     print(tab.name)
 
 # %%
-columns = ["Period", "Town Id", "Bua Buasd", "Size Band"]
+columns = ["Period", "Town Id", "Size Band"]
 
 # %%
 if tab.name == 'Table':
+    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
     print(tab.name)
     cell = tab.excel_ref("A1")
+    remove = tab.filter(contains_string("Total")).expand(DOWN)
 
 
-    period = cell.fill(DOWN)
+    period = cell.fill(DOWN).is_not_blank().is_not_whitespace()
+    trace.Period("Defined from cell A2 and down")
 
     town_id = cell.shift(1,0).fill(DOWN)
+    trace.Town_Id("Defined from cell B2 and down")
 
+    size_band = cell.shift(2,0).fill(RIGHT)-remove
+    trace.Size_Band("Defined from cell D1 and right excluding remove")
 
-    bua_buasd = cell.shift(2,0).fill(DOWN)
+    observations = size_band.waffle(town_id)-remove
 
-    size_band = cell.shift(2,0).fill(RIGHT)
-
-
-    observations = size_band.waffle(bua_buasd)
-#     savepreviewhtml(observations,fname=tab.name + "Preview.html")
-    
     dimensions =[
         HDim(period, "Period", DIRECTLY, LEFT),
         HDim(town_id, "Town Id", DIRECTLY, LEFT),
-        HDim(bua_buasd, "Bua Buasd", DIRECTLY, LEFT),
         HDim(size_band, "Size Band", DIRECTLY, ABOVE),
     ]
+    tidy_sheet = ConversionSegment(tab, dimensions, observations)
+    savepreviewhtml(tidy_sheet, fname=tab.name +'Preview.html')
+    trace.with_preview(tidy_sheet)
+    trace.store("combined_dataframe", tidy_sheet.topandas())
 
 # %%
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+df
+
+
+# %%
+def left(s, amount):
+    return s[:amount]
+def date_time (date):
+    if len(date)  == 6:
+        return 'year/'+ left(date, 4)
+df['Period'] =  df["Period"].apply(date_time)
+trace.Period("Period is formated to have pattern year/{year}")
+
+# %%
+df.rename(columns = {"OBS":"Value"}, inplace = True)
+
+# %%
+COLUMNS_TO_NOT_PATHIFY = ['Period', 'Value', 'Town Id']
+
+# %%
+for col in df.columns.values.tolist():
+    if col in COLUMNS_TO_NOT_PATHIFY:
+        continue
+    try:
+        df[col] = df[col].apply(pathify)
+    except Exception as err:
+        raise Exception('Failed to pathify column "{}".'.format(col)) from err
+
+# %%
+cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
+cubes.output_all()
+
+# %%
+trace.render("spec_v1.html")
