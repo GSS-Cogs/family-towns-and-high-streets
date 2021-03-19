87e0f229315564fe635313940f4984af79f0c199
RedWalters <jonathan.j.walters@gmail.com>
2021-03-19 11:13:51 +0000

    adding employment type codelist and everything that goes with that

--- datasets/ONS-Employment-for-towns-by-broad-industry-groups-Business-Register-and-Employment-Survey-BRES/main.py
+++ datasets/ONS-Employment-for-towns-by-broad-industry-groups-Business-Register-and-Employment-Survey-BRES/main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[35]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
 # +
 from gssutils import *
 import pandas as pd
 import json
+
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
 
 scraper = Scraper(seed = 'info.json')
 scraper
@@ -11,10 +26,17 @@ scraper
 cubes = Cubes("info.json")
 
 trace = TransformTrace()
-# -
+
+
+# In[36]:
+
 
 for i in scraper.distributions:
     display(i)
+
+
+# In[37]:
+
 
 distribution = scraper.distribution(title= lambda t: "Business Register and Employment Survey (BRES), 2018" in t)
 datasetTitle = distribution.title
@@ -26,45 +48,49 @@ tabs = distribution.as_databaker()
 for tab in tabs:
     print(tab.name)
 
-columns = ["Region", "Industry", "Period", "Value", "Marker", "Measure Type", "Unit"]
+columns = ["Region", "Industry", "Period"]
 
 tab_names_to_process = ["Employment_by_industry", "Part-time_employees", "Full-time_employees"]
 for tab_name in tab_names_to_process:
     if tab.name not in [x.name for x in tabs]:
         raise ValueError(f'Aborting. A tab named {tab_name} required but not found')
-    
+
         # Select the tab in question
     tab = [x for x in tabs if x.name == tab_name][0]
     print(tab.name)
     trace.start(datasetTitle, tab, columns, distribution.downloadURL)
     cell = tab.excel_ref("A2").is_not_blank().is_not_whitespace()
-    
+
     region = cell.fill(DOWN)
     trace.Region("Defined from cell A2 and down")
-    
+
     industry = cell.shift(1, 0).fill(RIGHT)
     trace.Industry("Defined from cell D2 and right")
-    
+
     period = 2018
     trace.Period("Hardcoded as 2018")
-    
+
     observations = industry.waffle(region)
 
-    
+
     dimensions = [
         HDim(region, "Region", DIRECTLY, LEFT),
         HDim(industry, "Industry", DIRECTLY, ABOVE),
-        HDimConst("Period", 2018)
+        HDimConst("Period", 2018),
+        HDimConst("Employment Type", tab.name)
     ]
     tidy_sheet = ConversionSegment(tab, dimensions, observations)
     savepreviewhtml(tidy_sheet, fname = tab.name+"Preview.html")
     trace.with_preview(tidy_sheet)
     trace.store("combined_dataframe", tidy_sheet.topandas())
 
+
+
+# In[38]:
+
+
 df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
 df
-
-df['DATAMARKER'].unique()
 
 df['Industry'] = df['Industry'].map(lambda x: 'mining-quarrying-utilities'
                                     if (x == 'Mining, quarrying & utilities (B,D and E)')
@@ -94,15 +120,42 @@ df = df.rename(columns={'OBS' : 'Value', 'DATAMARKER' : 'Marker'})
 df['Measure Type'] = 'count'
 df['Unit'] = 'person'
 
-df = df.replace({'Marker' : {'!' : 'suppressed'}})
+df = df.replace({'Marker' : {'!' : 'suppressed'},
+                 'Employment Type' : {'Employment_by_industry' : 'all',
+                                      'Full-time_employees' : 'full time',
+                                      'Part-time_employees' : 'part time'}})
 
 df['Period'] = df.apply(lambda x: 'year/' + str(x['Period']), axis = 1)
 
-df = df[['Period', 'Region', 'Industry', 'Value', 'Marker', 'Measure Type', 'Unit']]
+df['Value'] = df.apply(lambda x: left(str(x['Value']), len(str(x['Value'])) - 2) if 'suppressed' != x['Marker'] else x['Value'], axis = 1)
+
+#df = df[['Period', 'Region', 'Industry', 'Value', 'Marker', 'Measure Type', 'Unit']]
 
 df
+
+
+# In[39]:
+
 
 cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
 cubes.output_all()
 
 trace.render("spec_v1.html")
+
+
+# In[40]:
+
+
+from IPython.core.display import HTML
+for col in df:
+    if col not in ['Value']:
+        df[col] = df[col].astype('category')
+        display(HTML(f"<h2>{col}</h2>"))
+        display(df[col].cat.categories)
+
+
+# In[40]:
+
+
+
+
